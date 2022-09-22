595eb565b8d75ab67a9d925fd8330e726667fe05
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2022-09-22 11:54:37 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+console.log("Hello")
+let k = 5;
+
+console.log(k--);
+
+console.log(k)
+
+console.log(--k)
+
+console.log(5 == 3);
+
+console.log(4 ==4);
+
+console.log(5!=4);
+console.log(4!=4);
+
+console.log( 5> 4);
+
+console.log(4 >5);
+
+console.log(5< 4);
+console.log(4<5);
+
+console.log( 5<= 4);
+
+console.log(4 <=5);
