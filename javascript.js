cd577099f6c01544d7376a5447a6a0eaef4d7204
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2022-09-23 11:41:45 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -24,3 +24,26 @@ console.log(4<5);
 console.log( 5<= 4);
 
 console.log(4 <=5);
+
+let n = 12;
+
+n+= 12; // Equal to n = + 12
+console.log(n)
+
+n -= 4; // Equal to n = n- 4
+console.log(n);
+
+m *= 5; // Equal to n = n *5
+console.log(n)
+
+
+let y = 4;
+
+y/= 2; // Equal to y = y/2
+console.log(y);
+
+y %= 2; // Equal to y = y%2
+console.log(y)
+
+console.log(first>second?"True":"False");
+
