98fd96ae7ed66d83cd3a64515f500e31c4a09516
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2022-09-30 12:13:38 +0100

    Add files via upload

--- Assignment 1/Assignment 3/JavaScript Pt6.js
+++ Assignment 1/Assignment 3/JavaScript Pt6.js
@@ -1,8 +1,8 @@
 let choice = prompt("Type: 1, 2, or 3");
 if (choice == 1){
     console.log("Hello")
-}if else (choice == 2){
+}else if (choice == 2){
     console.log("Howdy")
-}if else (choice ==3){
+}else if (choice ==3){
     console.log("Greetings!")
 }
