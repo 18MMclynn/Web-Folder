24ca9cb184305be39fe2c89d334219f5182cd9c2
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2022-09-30 11:42:47 +0100

    Add files via upload

---
+++ JavaScript Test Pt.5.js
@@ -0,0 +1,21 @@
+let num1 = 1;
+let num2 = 2;
+if(num1+num2){
+let choice = prompt("Type: Add: Subtract: Divide: Multiply: "); 
+if(choice == "Add"){
+    let myFirstNumber = Number(prompt("Enter a 1st Number"));
+    let mySecondNumber = Number(prompt("Enter a 2nd Number"));
+    console.log("The Total Is: ", myFirstNumber + mySecondNumber)
+}else if(choice == "Subtract"){
+    let myFirstNumber = Number(prompt("Enter a 1st Number"));
+    let mySecondNumber = Number(prompt("Enter a 2nd Number"));
+    console.log("The Difference Is: ", myFirstNumber - mySecondNumber)
+}else if(choice == "Divide"){
+    let myFirstNumber = Number(prompt("Enter a 1st Number"));
+    let mySecondNumber = Number(prompt("Enter a 2nd Number"));
+    console.log("The Remainder Is: ", myFirstNumber / mySecondNumber)
+}else if(choice == "Multiply"){
+    let myFirstNumber = Number(prompt("Enter a 1st Number"));
+    let mySecondNumber = Number(prompt("Enter a 2nd Number"));
+    console.log("The Average Is: ", myFirstNumber * mySecondNumber)
+}
