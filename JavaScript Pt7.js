1d45e2e474e651dd422d6f4977fc220e8bf17a02
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2022-09-30 12:29:45 +0100

    Add files via upload

---
+++ JavaScript Pt7.js
@@ -0,0 +1,25 @@
+// My First JavaScript Program
+Console.log ("Hello");
+
+// My First JavaScript Program
+console.log (Hello");
+
+// My First JavaScript Program
+console.log "Hello ");
+
+// My First JavaScript Program
+console.log ("Hello")
+
+// My First JavaScript Program
+// console.log ("Hello");
+
+My First JavaScript Program
+console.log ("Hello");
+
+console.log ("Hello");
+console.log ("Welcome To JavaScript");
+
+/*
+console.log ("Hello");
+console.log ("Welcome To JavaScript");
+*/
