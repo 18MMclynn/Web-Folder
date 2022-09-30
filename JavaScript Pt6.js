a22ba763de3d3616d256f76f83c24608f0f5dfcd
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2022-09-30 12:30:04 +0100

    Delete JavaScript Pt6.js

--- JavaScript Pt6.js
+++
@@ -1,8 +0,0 @@
-let choice = prompt("Type: 1, 2, or 3");
-if (choice == 1){
-    console.log("Hello")
-}else if (choice == 2){
-    console.log("Howdy")
-}else if (choice ==3){
-    console.log("Greetings!")
-}
