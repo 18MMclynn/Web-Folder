fbf0f05049bb122bb362072db03a60e806bd292b
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2022-10-07 12:08:55 +0100

    Add files via upload

---
+++ If-Else & Switch Statement.js
@@ -0,0 +1,31 @@
+//1.
+
+let userchoice = prompt("Type a Letter");
+
+switch(userchoice){
+case 1:
+}if(userchoice == "A"){
+console.log("A is a Vowel")
+}else if(userchoice == "E"){
+console.log("E is a Vowel")
+}else if(userchoice == "I"){
+console.log("I is a Vowel")
+}else if(userchoice == "O"){
+console.log("O is a Vowel")
+}else if(userchoice == "U"){
+console.log("U is a Vowel")
+}else if(userchoice == "a"){
+console.log("a is a Vowel")
+}else if(userchoice == "e"){
+console.log("e is a Vowel")
+}else if(userchoice == "i"){
+console.log("i is a Vowel")
+}else if(userchoice == "o"){
+console.log("o is a Vowel")
+}else if(userchoice == "u"){
+console.log("u is a Vowel")
+}else{console.log("This letter is a Consonant")}
+
+//2.
+
+let userchoice = prompt("Divide a number with 5");
