96b93c80845fe7a99da52c5b1c515bff65ecdd9e
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2022-10-12 10:47:45 +0100

    Add files via upload

---
+++ Exercise 6.js
@@ -0,0 +1,10 @@
+//up to 100
+
+let count1=0;
+while (count1<101){
+if(count1%2 == 0){
+console.log(count1);
+    
+}count1 = count1+1;
+
+}
---
+++ while.js
@@ -0,0 +1,27 @@
+//while loop execute a statemnet or code
+//long as the expression is true
+
+let count=0;
+while(count<10){
+    console.log(count)
+;
+count++; //count = count+1
+
+}
+
+
+
+
+
+
+
+
+
+
+//DO WHILE LOOP
+//ALWAYS RUNS THE CODE
+//AT LEAST ONCE
+do{
+    connsole.log(count);
+    count++;
+}while(count<20);
