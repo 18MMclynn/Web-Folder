4829d6e01b7b9650c17db183dbfa38a3babbff58
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2023-09-18 15:16:19 +0100

    Add files via upload

---
+++ string.py
@@ -0,0 +1,9 @@
+username = input("enter name ")
+second_username = input("enter 2nd name ")
+store = len(username)
+print(type(username))
+print(type(second_username))
+whole_username = username + " " + second_username
+print(whole_username)
+len(username)
+print(username[:])
