35ae159085bef99d77ea01632af656b9bd597868
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2023-11-16 14:26:02 +0000

    Add files via upload

--- Python/Firebase.py
+++ Python/Firebase.py
@@ -7,3 +7,6 @@ cred = creedentials.Certificate(""
 firebase_admin.initialize_app(cred,{'databaseURL':
 
 ref = db.reference().child('classes').child('Biology123')
+ref.update.({'Description':'Biology123 class', 'id':'Biology','title':'Biology123'})
+ref = db.reference().child('classes').child('Math123')
+ref.update({'Description':'Math123','id':'Math123','title':'Math 123'})
