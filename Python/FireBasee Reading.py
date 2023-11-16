0c11533019d7697315f7d85e539736ff30aab41b
18MMclynn <113597317+18MMclynn@users.noreply.github.com>
2023-11-16 16:16:48 +0000

    Add files via upload

--- Python/FireBasee Reading.py
+++ Python/FireBasee Reading.py
@@ -4,16 +4,14 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 cred = credentials.Certificate("C:/Users/18MMcLynn.ACC/Desktop/Python/fir-1-eb8ba-firebase-adminsdk-vl3ez-89e6f3196b.json")
-
 firebase_admin.initialize_app(cred,{'databaseURL': 'https://fir-1-eb8ba-default-rtdb.europe-west1.firebasedatabase.app/'})
 
-ref = db.reference()
-ref.update({'classes':''})
 ref = db.reference().child('classes')
-ref.update({'Biology123':''})
-ref.update({'Maths123': ''})
 
-ref = db.reference().child('classes').child('Biology123')
-ref.update({'Description':'Biology123 class', 'id':'Biology123','title':'Biology 123'})
-ref = db.reference().child('classes').child('Math123')
-ref.update({'Description':'Math123','id':'Math123','title':'Math 123'})
+results = ref.get()
+#print(results)
+#print(type(results))
+for k,v in results.items():
+    print("Key is: ", k,"\t value is: ",v)
+    for k2, v2 in v.items():
+        print("Key is: ",k2, "\t value is: ",v2)
